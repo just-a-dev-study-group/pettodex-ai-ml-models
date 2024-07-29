df1da38c12f725474a5f00a97999ecf91ced9921
Frostzeichen <cruztjomar@gmail.com>
2024-07-29 16:28:29 +0800

    feat: disable csrf

--- visual/identify/views.py
+++ visual/identify/views.py
@@ -1,10 +1,23 @@
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
 def index(request):
-    return HttpResponse("The \"identify\" route lets you identify an animal species. Go to \"\\single\" to post a single image of an pet or \"\\bulk\" to post images by bulk.")
+    if request.method == "GET":
+        return HttpResponse("The \"identify\" route lets you identify an animal species. Go to \"\\single\" to post a single image of an pet or \"\\bulk\" to post images by bulk.")
+    elif request.method == "POST":
+        return HttpResponse("You have sent a POST request")
 
+@csrf_exempt
 def single(request):
-    return HttpResponse("Send an image here via a POST request to submit a singular image of a pet.")
+    if request.method == "GET":
+        return HttpResponse("Send an image here via a POST request to submit a singular image of a pet.")
+    elif request.method == "POST":
+        return HttpResponse("You have sent a POST request")
 
+@csrf_exempt
 def bulk(request):
-    return HttpResponse("Send a list or array of images here via a POST request to submit bulk images of pets.")
+    if request.method == "GET":
+        return HttpResponse("Send a list or array of images here via a POST request to submit bulk images of pets.")
+    elif request.method == "POST":
+        return HttpResponse("You have sent a POST request")
